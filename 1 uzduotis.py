3439ac35aea3071f244f28892a8075026530bf91
DudeOwski <96206160+DudeOwski@users.noreply.github.com>
2023-06-15 14:21:18 +0300

    Update 1 uzduotis.py

--- 1 uzduotis.py
+++ 1 uzduotis.py
@@ -1,13 +1,1 @@
-def likutis_po_pirkimo(kainos, pinigai):
-    min_likutis = pinigai
-      
-    for i in range(len(kainos)):
-        for j in range(i+1, len(kainos)):
-            
-            bendra_kaina = kainos[i] + kainos[j]
-            likutis = pinigai - bendra_kaina
-        
-            if likutis >= 0 and likutis < min_likutis:
-                min_likutis = likutis
 
-    return results
