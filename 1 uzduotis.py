ee691ace16de79e534131c03c2b6f981302402cd
DudeOwski <96206160+DudeOwski@users.noreply.github.com>
2023-06-15 12:30:16 +0300

    Add files via upload

---
+++ 1 uzduotis.py
@@ -0,0 +1,17 @@
+def likutis_po_pirkimo(kainos, pinigai):
+    kainos = [10, 20, 30]
+    pinigai = [50]
+    min_likutis = pinigai
+    results = [likutis_po_pirkimo]
+    
+    
+    for i in range(len(kainos)):
+        for j in range(i+1, len(kainos)):
+            
+            bendra_kaina = kainos[i] + kainos[j]
+            likutis = pinigai - bendra_kaina
+        
+            if likutis >= 0 and likutis < min_likutis:
+                min_likutis = likutis
+
+    return results
